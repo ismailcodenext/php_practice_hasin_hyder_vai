e819375ad60af7f011cd4ccd2a64689c13f5c2cc
codenextitD <engr.ismail348@gmail.com>
2023-03-10 23:47:49 +0600

    number fectorial

--- php_practice.php
+++ php_practice.php
@@ -1,9 +1,7 @@
 <?php
-  
-  for($i=10, $j=0; $i >=0; $i--, $j++){
-
-    echo $i. ":" .$j;
-
-    // echo $i. ":". (10-$i);
-    echo PHP_EOL;
+  $n=6;
+  for($i =$n, $fectorial=1; $i>1; $i--){
+    // $fectorial = $fectorial * $i;
+    $fectorial*=$i;
   }
+  printf("The Fectoril of %d is %d", $n, $fectorial);

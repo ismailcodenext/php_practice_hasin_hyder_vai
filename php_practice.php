e890ff671c9d935c5be8d425e34794d0d90e6706
codenextitD <engr.ismail348@gmail.com>
2023-03-10 23:08:43 +0600

    for loop multiple staping

--- php_practice.php
+++ php_practice.php
@@ -1,23 +1,9 @@
 <?php
   
-  for($i=0; $i<=10; $i++){
+  for($i=10, $j=0; $i >=0; $i--, $j++){
+
+    echo $i. ":" .$j;
+
+    // echo $i. ":". (10-$i);
     echo PHP_EOL;
-    for($j=0; $j<=$i; $j++){
-      
-      echo "*";
-      
-    }
-    
   }
-  echo PHP_EOL;
-  $i =0;
-  while($i<=10){
-    $i++;
-    echo $i.PHP_EOL;
-  }
-  echo PHP_EOL;
-  $i = 0;
-  do{
-    $i++;
-    echo $i.PHP_EOL;
-  }while($i<=10);
